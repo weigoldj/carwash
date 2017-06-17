f706792a76fa9a7b77c87836064a6d3f56244c44
Johann Weigold <johann.weigold@icloud.com>
2017-06-17 14:40:15 -0500

    add basic transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,14 @@
 class Transaction < ApplicationRecord
+  
+  # Type of Vehicles the car wash can wash
+  VEHICLES = %w(Car Truck)
+  
+  # validations
+  validates :license_plate, presence: true
+  validates :vehicle_type, :inclusion=> { :in => VEHICLES }
+  
+  # things that price and transaction are based on.
   attr_accessor :bed_down, :mud_in_bed, :vehicle_type
+  
+ 
 end
